15b3b1cec6b2606309d4c3f093db05f9401d1ad0
santhanakrishnansk <techimove@gmail.com>
2023-03-28 10:33:21 +0530

    New Commit

--- react-axios-frontend-backend/server/server.js
+++ react-axios-frontend-backend/server/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-app.use(cors());
+const corsOption = {
+  origin: "http://localhost:3000",
+  Credential: "true",
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOption));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -13,7 +18,7 @@ app.post("/post", (request, response) => {
   console.log({ userName, password, email, phoneNumber });
 
   return response.status(200).json({message : "Success"});
-});
+}); 
 
 const PORT = 1000;
 app.listen(PORT, () => {
